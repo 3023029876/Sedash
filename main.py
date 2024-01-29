feb6eeecaffc7efa4748d3fe4dcd8fa025d66a09
3023029876 <azaliya_salikhova06@mail.ru>
2024-01-29 10:03:08 +0300

    задание 1

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import openpyxl
-book =openpyxl.open('ТАКСИ ЗАДАНИЕ 1.xlsx',read_only=True)
-sheet = book.active
-for row in range(1,17):
-    driver = sheet[row][0].value
-    car = sheet[row][1].value
-    raiting = sheet[row][2].value
-    category = sheet[row][3].value
-    print (driver,car,raiting,category)
+import pandas as pd
+
+
+df=pd.read_excel(
+    io="ТАКСИ ЗАДАНИЕ 1.xlsx",
+    engine="openpyxl",
+    sheet_name="Лист1",
+    skiprows=0,
+    usecols="A:D",
+    nrows=16,
+)
+print(df)
+
+
+
+
+
+
